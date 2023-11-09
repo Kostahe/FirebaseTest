a0a74bad977cb1a994d888726eae3f533f61d23c
Konstantyn <gagarin2789@gmail.com>
2023-11-09 16:48:53 +0100

    Added dependencies for firebase

--- app/src/main/java/com/example/firebaseapp/MainActivity.kt
+++ app/src/main/java/com/example/firebaseapp/MainActivity.kt
@@ -2,10 +2,43 @@ package com.example.firebaseapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.database
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var database: DatabaseReference
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val textView: TextView = findViewById(R.id.text_view)
+
+        database = Firebase.database.reference
+        Log.d("Tag", database.toString())
+        database.child("price").setValue("1940$")
+
+        val postListener = object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val goldPrice = snapshot.value as? String
+
+                textView.text = goldPrice
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+
+            }
+
+        }
+
+        database.child("price").addValueEventListener(postListener)
     }
 }
