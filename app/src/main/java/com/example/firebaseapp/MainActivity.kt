874eb14bbd0f53bd14bad9991081f5745184e11e
Konstantyn <gagarin2789@gmail.com>
2023-11-09 18:32:25 +0100

    FireStore added maps

--- app/src/main/java/com/example/firebaseapp/MainActivity.kt
+++ app/src/main/java/com/example/firebaseapp/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.firebaseapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 
@@ -13,7 +14,27 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val textView: TextView
+
         val database = Firebase.firestore
+
+        val usersCollection = database.collection("Users")
+
+        val user1 = hashMapOf(
+            "name" to "jack",
+            "last" to "reacher",
+            "bord" to "1992"
+        )
+
+        val user2 = hashMapOf(
+            "name" to "john",
+            "last" to "travolta",
+            "bord" to "1982"
+        )
+
+        // Adding Documents to Collection
+        usersCollection.document("user1").set(user1)
+        usersCollection.document("user2").set(user2)
 
 
     }
